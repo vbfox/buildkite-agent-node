c43e8b7926e15675dc658845755b58e5ef71ac02
Julien Roncaglia <julien@roncaglia.fr>
2020-05-09 15:33:10 +0200

    Fix artifact name

--- .buildkite/buildkite.sh
+++ .buildkite/buildkite.sh
@@ -5,5 +5,5 @@ yarn run -s build
 node .buildkite/tests/annotations.js
 
 #find . \( -type f -path "./dist" -or -type f -path "./.buildkite/tests" -or -path "./node_modules" \) -exec tar czvf compiled.tar.gz {} +
-tar czvf compiled.tar.gz "./node_modules" "./.buildkite/tests" "./dist"
+tar czf compiled.tgz "./node_modules" "./.buildkite/tests" "./dist"
 buildkite-agent artifact upload compiled.tgz
